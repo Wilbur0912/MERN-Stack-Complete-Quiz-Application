f60a5931829264e4759a5be2cc82f55e212b639f
Weber <b10923010@gemail.yuntech.edu.tw>
2023-01-02 17:06:00 +0800

    Main Component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import '../styles/Main.css'
 
 export default function Main(){
+
+    const inputRef = useRef(null)
     return(
-        <div>Main</div>
+        <div className='container'>
+            <h1 className='title text-light'>Quiz Application</h1>
+            <ol>
+                <li>You will be asked 10 questions one after another.</li>
+                <li>Each question has three options. you can choose only one options.</li>
+                <li>You can review and change answers before the quiz finish.</li>
+                <li>The result will be declared at the end of the quiz.</li>
+            </ol>
+
+            <form id="form">
+                <input ref={inputRef} className="userid" type="text" placeholder="Username*" />
+            </form>
+
+            <div className="start">
+                <Link className="btn" to={'quiz'}>Start Quiz</Link>
+            </div>
+        </div>
     )
 }
