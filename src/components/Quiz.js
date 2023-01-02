b36c61356151f9aeb8cf8fa36d1a6e4d03298181
Weber <b10923010@gemail.yuntech.edu.tw>
2023-01-02 17:57:14 +0800

    quiz_component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,27 @@
 import React from 'react'
 
 export default function Quiz() {
+
+
+  /** next button event handler*/
+  function onNext(){
+    console.log('On next click')
+  }
+
+  /** Prev button event handler*/
+  function onPrev(){
+    console.log('On onPrev click')
+  }
+
   return (
-    <div>Quiz</div>
+    <div className='container'>
+      <h1 className='title text-light'>Quiz Application </h1>
+      {/**display questions */}
+      <div className='grid'>
+        <button className='btn prev' onClick={onPrev}>Prev</button>
+        <button className='btn next' onClick={onNext}>Next</button>
+      </div>
+    </div>
   )
 }
 
