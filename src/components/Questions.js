f35a244103d1a956d8b514e03412f98d10bf52e2
Weber <b10923010@gemail.yuntech.edu.tw>
2023-01-15 20:21:31 +0800

    question componet

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,8 +1,17 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import data from '../database/data'
+
 
 export default function Questions() {
 
     const [checked, setChecked] = useState(undefined)
+
+    const question = data[0]
+
+    useEffect(() => {
+        console.log(data);
+    })
+
 
     function onSelect() {
         console.log('radio button change')
@@ -10,21 +19,25 @@ export default function Questions() {
 
     return (
         <div className='questions'>
-            <h2 className='text-light'>Simple Question 1</h2>
+            <h2 className='text-light'>{question.question}</h2>
 
-            <ul>
-                <li>
-                    <input
-                        type="radio"
-                        value={true}
-                        name="options"
-                        id='q1-option'
-                        onChange={onSelect()}
-                    />
-
-                    <label className='text-primary' htmlFor="q1-option"></label>
-                    <div className='check checked'></div>
-                </li>
+            <ul key={question.id}>
+                {
+                    question.options.map((q,i) => (
+                    <li key={i}>
+                        <input
+                            type="radio"
+                            value={true}
+                            name="options"
+                            id={`q${i}-option`}
+                            onChange={onSelect()}
+                        />
+    
+                        <label className='text-primary' htmlFor={`q${i}-option`}>{q}</label>
+                        <div className='check'></div>
+                    </li>
+                    ))
+                }
             </ul>
         </div>
     )
