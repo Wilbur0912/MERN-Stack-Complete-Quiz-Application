4f88da33a3228dbe9b582ec5686ede54a3de99e2
Weber <b10923010@gemail.yuntech.edu.tw>
2023-01-15 21:05:32 +0800

    reducer

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+/** call reducers */
+import questionsReducer from './question_reducer';
+import { resultReducer } from './result_reducer';
+
+const rootReducer = combineReducers({
+    questions : questionsReducer,
+    result : resultReducer
+})
+
+/**create store with reducer */
+export default configureStore({reducer : rootReducer})
