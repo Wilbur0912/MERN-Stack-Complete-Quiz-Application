66f9f2b1766d9166dd4a6c201d2ea1d5da1de739
Weber <b10923010@gemail.yuntech.edu.tw>
2023-01-20 11:06:48 +0800

    pushResultArray

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,18 +3,19 @@ import React, {useEffect} from 'react'
 import Questions from './Questions'
 
 import { moveNextQuestion, movePrevQuestion } from '../hooks/FetchQuestion';
+import { PushAnswer } from '../hooks/setResult';
 
 /** redux store import */
 import {useSelector, useDispatch} from 'react-redux'
 import { movePrevAction } from '../redux/question_reducer';
 export default function Quiz() {
 
-  //const trace = useSelector(state => state.questions.trace);
+  const state = useSelector(state => state);
   const {queue, trace} = useSelector(state => state.questions);
   const dispatch = useDispatch();
 
   useEffect(() =>{
-    console.log(queue)
+    console.log(state)
   })
   /** next button event handler*/
   function onNext(){
@@ -22,6 +23,8 @@ export default function Quiz() {
 
     if(trace < (queue.length)){
       dispatch(moveNextQuestion());
+
+      dispatch(PushAnswer(1));
     }
     /**update the trace value by one using MoveNextAction */
   }
